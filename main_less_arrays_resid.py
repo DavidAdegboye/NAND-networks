4383b21f220f0814e69fadd9ce5494a875706bbe
david <davidadegboye26@gmail.com>
2025-03-31 20:25:52 +0100

    l5_coeff

--- main_less_arrays_resid.py
+++ main_less_arrays_resid.py
@@ -1108,7 +1108,7 @@ def run(timeout=config["timeout"]):
                                     output[batch*batch_size:(batch+1)*batch_size],
                                     accuracy[1][batch*batch_size:(batch+1)*batch_size],
                                     accuracy[2][batch*batch_size:(batch+1)*batch_size],
-                                    max_fan_in, max_gates)
+                                    max_fan_in, max_gates, l5_coeff)
                     updates, opt_state = solver.update(gradients, opt_state, neurons)
                     neurons = optax.apply_updates(neurons, updates)
                     accuracy = acc(neurons)
@@ -1116,7 +1116,7 @@ def run(timeout=config["timeout"]):
                     gradients = grad(neurons,
                                     inputs[batch*batch_size:(batch+1)*batch_size],
                                     output[batch*batch_size:(batch+1)*batch_size],
-                                    jnp.array([]), jnp.array([]), max_fan_in, max_gates)
+                                    jnp.array([]), jnp.array([]), max_fan_in, max_gates, l5_coeff)
                     updates, opt_state = solver.update(gradients, opt_state, neurons)
                     neurons = optax.apply_updates(neurons, updates)
             if time.time() - start_run_time > timeout * 60:
