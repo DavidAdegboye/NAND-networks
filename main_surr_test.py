1d42b8449e21ce31c0dbcf7e58950511a50a1f58
david <davidadegboye26@gmail.com>
2025-04-03 15:19:33 +0100

    test

--- main_surr_test.py
+++ main_surr_test.py
@@ -374,52 +374,57 @@ def output_circuit(neurons: Network, verbose=True, super_verbose=False) -> List[
         for layer in true_arch:
             sum_arch.append(sums)
             sums += layer
-        print("surrogates")
         for layer_i in range(i_1):
             gates.append([])
             gate_i1 = layer_i+1
             gate_i2 = 0
-            for neuron_i in range(len(surr_arr[layer_i])):
-                connected: List[Tuple[int, str]] = []
-                for inner_layer_i, weight_i in surr_arr[layer_i][neuron_i]:
-                    i = sum_arch[inner_layer_i] + int(weight_i)
-                    connected.append((indices[i], circuits[indices[i]]))
-                added += 1
-                connecteds.append([node[0] for node in connected])
-                i = len(connecteds)-1
-                if super_verbose:
-                    print(i, connecteds[i])
-                if len(connected) == 1:
-                    node = '¬' + connected[0][1]
-                    if len(node) > 2:
-                        if node[:3] == "¬¬¬":
-                            node = node[2:]
-                else:
-                    node = '¬(' + '.'.join([element[1] for element in connected]) + ')'
-                if node in c2i.keys():
-                    if layer_i == i_1-1:
+            print("surrogates")
+            if layer_i < len(surr_arr):
+                for neuron_i in range(len(surr_arr[layer_i])):
+                    connected: List[Tuple[int, str]] = []
+                    for inner_layer_i, weight_i in surr_arr[layer_i][neuron_i]:
+                        i = sum_arch[inner_layer_i] + int(weight_i)
+                        connected.append((indices[i], circuits[indices[i]]))
+                    added += 1
+                    connecteds.append([node[0] for node in connected])
+                    i = len(connecteds)-1
+                    if super_verbose:
+                        print(i, connecteds[i])
+                    if len(connected) == 1:
+                        node = '¬' + connected[0][1]
+                        if len(node) > 2:
+                            if node[:3] == "¬¬¬":
+                                node = node[2:]
+                    else:
+                        node = '¬(' + '.'.join([element[1] for element in connected]) + ')'
+                    if node in c2i.keys():
+                        if layer_i == i_1-1:
+                            circuits.append(node)
+                            gates[-1].append(["=", index2gate[c2i[node]]])
+                            index2gate[added] = (gate_i1, gate_i2)
+                            gate_i2 += 1
+                            for prev_node in connected:
+                                used.add(prev_node[0])
+                        else:
+                            circuits.append('_')
+                        indices[added] = c2i[node]
+                    else:
                         circuits.append(node)
-                        gates[-1].append(["=", index2gate[c2i[node]]])
+                        c2i[node] = added
+                        indices[added] = added
+                        gates[-1].append([index2gate[element[0]] for element in connected])
                         index2gate[added] = (gate_i1, gate_i2)
                         gate_i2 += 1
-                        for prev_node in connected:
-                            used.add(prev_node[0])
-                    else:
-                        circuits.append('_')
-                    indices[added] = c2i[node]
-                else:
-                    circuits.append(node)
-                    c2i[node] = added
-                    indices[added] = added
-                    gates[-1].append([index2gate[element[0]] for element in connected])
-                    index2gate[added] = (gate_i1, gate_i2)
-                    gate_i2 += 1
-                    if layer_i == i_1-1:
-                        for prev_node in connected:
-                            used.add(prev_node[0])
-                        used.add(added)
+                        if layer_i == i_1-1:
+                            for prev_node in connected:
+                                used.add(prev_node[0])
+                            used.add(added)
             print("natties")
-            for neuron_i in range(len(shapes[layer_i])):
+            if layer_i < len(surr_arr):
+                start = len(surr_arr[layer_i])
+            else:
+                start = 0
+            for neuron_i in range(start, len(shapes[layer_i])):
                 i = 0
                 connected: Set[Tuple[int, str]] = set()
                 for inner_layer_i in range(len(shapes[layer_i][neuron_i])):
