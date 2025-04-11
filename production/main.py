485dca466974e4a49ae97c71d114e53acc6020d1
david <davidadegboye26@gmail.com>
2025-04-11 11:29:11 +0100

    adding production code, big refactor

--- production/main.py
+++ production/main.py
@@ -393,6 +393,8 @@ def forward_conv(
 forward_conv_cont = jax.jit(partial(forward_conv, and_helper_func=and_cont))
 forward_conv_disc = jax.jit(partial(forward_conv, and_helper_func=and_disc))
 
+print(type(forward_conv_disc))
+
 @partial(jax.jit, static_argnames=("convs", "forward_conv_func"))
 def feed_forward_conv(
     xs: jnp.ndarray,
@@ -415,7 +417,7 @@ def feed_forward_conv(
     the dense layers
     """
     for i, (ws, (_,_,s,n)) in enumerate(zip(weights, convs)):
-        temp = forward_conv_func(xs, ws, s, 27)
+        temp = forward_conv_func(xs, ws, s, n)
         xs = jnp.concatenate(
             [imgs_list[i], 1-imgs_list[i], temp, 1-temp], axis=0)
     return xs
