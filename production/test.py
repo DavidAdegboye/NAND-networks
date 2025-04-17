79f86a1bd42d962825bea4e9ace68131e2c4c637
david <davidadegboye26@gmail.com>
2025-04-17 02:02:02 +0100

    adding test.py

--- production/test.py
+++ production/test.py
@@ -20,7 +20,7 @@ def run_test(updates: Dict[str, any]):
     with open("set-up.yaml", "r") as f:
         config = yaml.safe_load(f)
 
-    print(type(updates))
+    print(updates.keys())
     for k,v in updates.keys():
         config[k] = v
 
