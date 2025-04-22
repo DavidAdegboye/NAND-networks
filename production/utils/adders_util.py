b9417a18cd90e978957a42fa082dd9a861285fcd
david <davidadegboye26@gmail.com>
2025-04-22 01:25:41 +0100

    adding test.py

--- production/utils/adders_util.py
+++ production/utils/adders_util.py
@@ -23,10 +23,15 @@ def set_up_adders(config_dict) -> Tuple[jnp.ndarray, jnp.ndarray, int, int, int]
     num_ins = 2**ins
     inputs = jax.vmap(denary_to_binary_array, in_axes=(0, None))(
         jnp.arange(num_ins), bits*2)
-    out_bits = config["out_bits"]
-    output = jax.vmap(get_output)(jnp.arange(num_ins))[:,:out_bits]
-    print(output.shape)
-    return inputs, output, ins, out_bits, num_ins
+    if config["specific"]:
+        output = jax.vmap(get_output)(jnp.arange(num_ins))[:,config["first_i"]:config["last_i"]]
+        print(output.shape)
+        return inputs, output, ins, config["last_i"]-config["first_i"], num_ins
+    else:
+        out_bits = config["out_bits"]
+        output = jax.vmap(get_output)(jnp.arange(num_ins))[:,:out_bits]
+        print(output.shape)
+        return inputs, output, ins, out_bits, num_ins
 
 # function to convert a denary number to a a jnp array of bits
 def denary_to_binary_array(number: int, bits: int) -> jnp.ndarray:
