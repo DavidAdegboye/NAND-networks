d6a4e3a1cc17a7c970c30a636405db382853a3bf
david <davidadegboye26@gmail.com>
2025-04-22 18:23:46 +0100

    adding test.py

--- production/main.py
+++ production/main.py
@@ -1342,8 +1342,8 @@ def test_fan_in_rand(weights: Network) -> bool:
             [print(circ) for circ in (output_circuit(weights, True, True, "rand"))]
             print(f"Max fan-in ({temp}) not good enough")
             current_max_fan_in_rand = temp
-        else:
-            [print(circ) for circ in (output_circuit(weights, False, False, "rand"))]
+        # else:
+        #     [print(circ) for circ in (output_circuit(weights, False, False, "rand"))]
         return False
     return True
 
