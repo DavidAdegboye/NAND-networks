11a83aa306270523509d6d82932dc190bdb4e87f
david <davidadegboye26@gmail.com>
2025-04-26 12:28:57 +0100

    adding test.py

--- production/test.py
+++ production/test.py
@@ -1833,7 +1833,7 @@ with open(config["output_file"], "a") as f:
 archs = [[2048], [1024, 768, 512, 256]]
 arch = [1024, 768, 512, 256]
 pools = [[], [[3, 1, "max"], [3, 1, "min"]]]
-pf = [[3, 1, "max"], [3, 1, "min"]]
+pf = []
 mgms = [1]
 # max_fans = [0, 32, 64, 96, 128]
 for mgm in mgms:
