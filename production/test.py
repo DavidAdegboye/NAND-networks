7c2e691b2d09bb486595dd20c1c019c9c52b5242
david <davidadegboye26@gmail.com>
2025-05-04 19:35:06 +0100

    adding test.py

--- production/test.py
+++ production/test.py
@@ -1862,20 +1862,19 @@ with open(config["output_file"], "a") as f:
 """
 
 for num_layers in range(6, 8):
-    for width_mult in range(1,3):
-        run_start = time.time()
-        arch = [width_mult*256*layer_i for layer_i in range(num_layers, 0, -1)]
-        min_gates = [1568] + arch.copy() + [10]
-        min_gates = [layer-2 for layer in min_gates]
-        run_test({"min_gates_used_penalty_coeff": 1,
-                "min_gates": min_gates,
-                "pool_filters": [],
-                "architecture": arch})
-        run_end = time.time()
-        with open("set-up.yaml", "r") as f:
-            config = yaml.safe_load(f)
-        with open(config["output_file"], "a") as f:
-            f.write(f"Total time for test: {run_end - run_start} seconds.\n")
+    run_start = time.time()
+    arch = [128*layer_i for layer_i in range(num_layers, 0, -1)]
+    min_gates = [1568] + arch.copy() + [10]
+    min_gates = [layer-2 for layer in min_gates]
+    run_test({"min_gates_used_penalty_coeff": 1,
+            "min_gates": min_gates,
+            "pool_filters": [],
+            "architecture": arch})
+    run_end = time.time()
+    with open("set-up.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    with open(config["output_file"], "a") as f:
+        f.write(f"Total time for test: {run_end - run_start} seconds.\n")
 true_end = time.time()
 with open("set-up.yaml", "r") as f:
     config = yaml.safe_load(f)
