74506ba29ea81b7756e17fe36b94cdbc645cf52c
david <davidadegboye26@gmail.com>
2025-05-07 14:22:46 +0100

    adding test.py

--- clean/customtest.py
+++ clean/customtest.py
@@ -24,34 +24,38 @@ with open(config["output_file"], "w") as f:
 
 for npn_class in npn_classes:
     run_start = time.time()
-    try:
-        if not main.run_test({"output": [[int(entry)] for entry in npn_class]},
-                              "set-up-custom.yaml"):
+    success = False
+    attempts = 0
+    while not success:
+        try:
+            attempts += 1
             with open(config["output_file"], "a") as f:
-                f.write(f"Retrying\n")
-            main.run_test({"output": [[int(entry)] for entry in npn_class]},
-                           "set-up-custom.yaml")
-    except KeyboardInterrupt:
-        break
-    except Exception as e:
-        with open(config["output_file"], "a") as f:
-            f.write(f'Error "{e}" with following truth table:\n{npn_class}\n')
+                f.write(f"Attempt {attempts:}\n")
+            success = main.run_test({"output": [[int(entry)] for entry in npn_class]},
+                                "set-up-custom.yaml")
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            with open(config["output_file"], "a") as f:
+                f.write(f'Error "{e}" with following truth table:\n{npn_class}\n')
     run_end = time.time()
     with open(config["output_file"], "a") as f:
         f.write(f"Total time for test: {run_end - run_start} seconds.\n")
     run_start = time.time()
-    try:
-        if not main.run_test({"output": [[1-int(entry)] for entry in npn_class]},
-                              "set-up-custom.yaml"):
+    success = False
+    attempts = 0
+    while not success:
+        try:
+            attempts += 1
             with open(config["output_file"], "a") as f:
-                f.write(f"Retrying\n")
-            main.run_test({"output": [[1-int(entry)] for entry in npn_class]},
-                           "set-up-custom.yaml")
-    except KeyboardInterrupt:
-        break
-    except Exception as e:
-        with open(config["output_file"], "a") as f:
-            f.write(f'Error "{e}" with following truth table:\n{tuple([not entry for entry in npn_class])}\n')
+                f.write(f"Attempt {attempts:}\n")
+            success = main.run_test({"output": [[1-int(entry)] for entry in npn_class]},
+                                "set-up-custom.yaml")
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            with open(config["output_file"], "a") as f:
+                f.write(f'Error "{e}" with following truth table:\n{[not entry for entry in npn_class]}\n')
     run_end = time.time()
     with open(config["output_file"], "a") as f:
         f.write(f"Total time for test: {run_end - run_start} seconds.\n")
