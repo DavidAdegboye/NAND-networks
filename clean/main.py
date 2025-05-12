7769f9760c24ffb4fe9afbb9cb689516b3213d13
david <davidadegboye26@gmail.com>
2025-05-12 17:35:41 +0100

    adding test.py

--- clean/main.py
+++ clean/main.py
@@ -1719,7 +1719,7 @@ def run_test(variables: Dict[str, any], config_file: str):
                         batch_size, batches,
                         inputs=inputs, output=output, scaled=scaled_train_imgs)
                     new_bce = batch_comp(
-                        partial(loss_conv, network=[weights, weights_conv]),
+                        partial(bce_loss_conv, network=[weights, weights_conv]),
                         batch_size, batches,
                         inputs=inputs, output=output, scaled=scaled_train_imgs)
                     accs.append(float(accuracy))
