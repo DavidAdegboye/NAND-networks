e5eaf24d111e9bb8a9b15ee1ed9ebc5cc682f71d
david <davidadegboye26@gmail.com>
2025-05-14 13:34:44 +0100

    typing

--- clean/tests/imgtest.py
+++ clean/tests/imgtest.py
@@ -26,6 +26,30 @@ ALL_KS = [1.0, 1.0, 1.0, 0.995, 0.99, 0.98, 0.97, 0.955, 0.94, 0.92, 0.91,
 ks = {s:k for (s,k) in zip(ALL_SIGMAS, ALL_KS)}
 distributions = ["beta_sampler", "normal_sampler1", "normal_sampler2"]
 
+lrs = [0.1, 0.03, 0.01, 0.003, 0.001]
+batchess = [1000, 2000, 5000, 10000]
+min_gatess = [[round(0.75*i) for i in [1568, 1536, 1280, 1024, 768, 512, 256]] + [10],
+              [0, 0, 0, 0, 0, 0, round(0.75*256), 0]]
+
+for lr, batches, min_gates in zip (lrs, batchess, min_gatess):
+    run_start = time.time()
+    try:
+        main.run_test({"lr_dense": [lr],
+                    "batches": batches,
+                    "min_gates": min_gates,
+                    "architecture": [1536, 1280, 1024, 768, 512, 256],
+                    "timeout": 0,}, "../configs/set-up.yaml")
+    except:
+        with open("../test_results/"+config["output_file"], "a") as f:
+            f.write(f"Memory overflow with {batches} batches.")
+    run_end = time.time()
+    with open("../test_results/"+config["output_file"], "a") as f:
+        f.write(f"Total time for test: {run_end - run_start} seconds.\n")
+true_end = time.time()
+with open("../test_results/"+config["output_file"], "a") as f:
+    f.write(f"Total time for 20 tests: {true_end - true_start} seconds.\n")
+
+"""
 architectures = [[1536, 1280, 1024, 768, 512, 256], [2048, 1024], [2048]]
 min_gatess = [[round(0.75*i) for i in [1568, 1536, 1280, 1024, 768, 512, 256]] + [10], [round(0.9*i) for i in [1568, 2048, 1024]] + [10], [0,0,0]]
 timeouts = [8*60, 8*60, 8*60]
@@ -44,3 +68,4 @@ for arch, min_gates, mgupc, timeout in zip (architectures, min_gatess, mgupcs, t
 true_end = time.time()
 with open("../test_results/"+config["output_file"], "a") as f:
     f.write(f"Total time for 20 tests: {true_end - true_start} seconds.\n")
+"""
